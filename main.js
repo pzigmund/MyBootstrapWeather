8d9cfcff8c3f202b65ea80b8436a8211193021bd
pzigmund <82932005+pzigmund@users.noreply.github.com>
2021-04-21 09:19:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(name){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
+    .then(response => response.json())
+    .then(data => {
+      var tempValue = data['main']['temp'];
+      var nameValue = data['name'];
+      var descValue = data['weather'][0]['description'];
+    
+      main.innerHTML = nameValue;
+      desc.innerHTML = "Desc - "+descValue;
+      temp.innerHTML = "Temp - "+tempValue;
+      input.value ="";
+    
+    })
+
+    .catch(err => alert("Wrong city name!"));
+})
+function getCoordintes() {
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+  
+    function success(pos) {
+        var crd = pos.coords;
+        var lat = crd.latitude.toString();
+        var lng = crd.longitude.toString();
+        var coordinates = [lat, lng];
+        console.log(`Latitude: ${lat}, Longitude: ${lng}`); 
+        getCity(coordinates);
+        return;
+  
+    }
+  
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+  
+    navigator.geolocation.getCurrentPosition(success, error, options);
+}
+function getCity(coordinates) {
+    var xhr = new XMLHttpRequest();
+    var lat = coordinates[0];
+    var lng = coordinates[1];
+  
+    xhr.open('GET', "https://us1.locationiq.com/v1/reverse.php?key=pk.faf89031dbbf489bd2fd386d5fd5b9a7&lat=" +lat + "&lon=" + lng + "&format=json", true);
+    xhr.send();
+    xhr.onreadystatechange = processRequest;
+    xhr.addEventListener("readystatechange", processRequest, false);
+  
+    function processRequest(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            var city = response.address.city;
+            var city2 = response.address.suburb;
+            var city3 = response.address.country;
+            console.log(city);
+            return;
+        }
+    }
+}
