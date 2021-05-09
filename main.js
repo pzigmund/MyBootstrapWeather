c150920e37a9db6598c7e02b9bd113b5b24e7f26
pzigmund <82932005+pzigmund@users.noreply.github.com>
2021-05-09 13:16:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,27 +3,143 @@ const button_locate = document.querySelector('#locate');
 const inputEl = document.querySelector('#input');
 var API_key_weather = '8beeaebc4fe0055b9a899d663f148a31';
 var API_key_iq = 'pk.faf89031dbbf489bd2fd386d5fd5b9a7';
+
+
 var lat = '';
 var lon = '';
-var city = '';
 var mycoordinates = new Array();
+
+var city = '';
 var temp = '';
 var popis = '';
-function shower(){
-    document.getElementById("dnes").innerHTML = temp+ ' ' + popis+ ' ' + city;
-    document.getElementById("input").value = '';
+var dt = '';
+
+var day_plus_one_temp;
+var day_plus_one_desc;
+var day_plus_two_temp;
+var day_plus_two_desc;
+var datum;
+var datum1;
+var datum2;
+let history = [];
+/*
+function timeConverter(){
+    var a = new Date(dt * 1000);
+    var months = ['Leden','Únor','Březen','Duben','Květen','Červen','Červenec','Srpen','Záři','Říjen','Listopad','Prosinec'];
+   // var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var month_n = a.getMonth();
+    var date = a.getDate();
+   // var hour = a.getHours();
+   // var min = a.getMinutes();
+   // var sec = a.getSeconds();
+   // var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+      var time = date + ' ' + month + ' ' + year;
+      console.log(time);
+    return time;
+  }
+  */
+function timeConverter2(timestamp) {
+    var a = new Date(timestamp * 1000);
+    var month_n = a.getMonth();
+    var date = a.getDate();
+    var time = [date, month_n];
+    //console.log(time);
+    return time;
+}
+function shower_history(){
+    if(localStorage.getItem('history')){
+        history = JSON.parse(localStorage.getItem('history'));
+        var moje_hisotrie = '';
+        for (let index = 0; index < history.length; index++) {
+            moje_hisotrie = moje_hisotrie + ' ' + history[index].date + ' ' + history[index].temperature + '°C' + ' ' + '<b>' +history[index].description + '</b>' + ' '  + history[index].city + '<br>' + '<br>'; }
+        document.getElementById("history").innerHTML = moje_hisotrie;
+
+    }
+}
+
+function shower() {
+    document.getElementById("prvni_1").innerHTML = temp + '°C';
+    document.getElementById("prvni_2").innerHTML = popis;
+    document.getElementById("prvni_3").innerHTML = datum;
+
+    document.getElementById("druhy_1").innerHTML = day_plus_one_temp + '°C';
+    document.getElementById("druhy_2").innerHTML = day_plus_one_desc;
+    document.getElementById("druhy_3").innerHTML = datum1;
+
+    document.getElementById("treti_1").innerHTML = day_plus_two_temp + '°C';
+    document.getElementById("treti_2").innerHTML = day_plus_two_desc;
+    document.getElementById("treti_3").innerHTML = datum2;
+
+    document.getElementById("input").value = city;
+
+   history.unshift({
+        temperature: temp ,
+        description: popis ,
+        date: datum ,
+        city: city
+    });
+
+    localStorage.setItem('history', JSON.stringify(history));
+    shower_history();
+
 }
 function getCity() {
-    var lat2 = mycoordinates[0];
-    var lon2 = mycoordinates[1];
+    lat = mycoordinates[0];
+    lon = mycoordinates[1];
 
-    fetch('https://eu1.locationiq.com/v1/reverse.php?key=' + API_key_iq + '&lat=' + lat2 + '&lon=' + lon2 + '&format=json')
+    fetch('https://eu1.locationiq.com/v1/reverse.php?key=' + API_key_iq + '&lat=' + lat + '&lon=' + lon + '&format=json')
         .then(response => response.json())
         .then(data => {
             var city2 = data.display_name;
             city = city2;
             shower();
         })
+
+}
+function get7day() {
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=daily&appid=' + API_key_weather + '&lang=cz')
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data);
+            // var city_name = data.name; //city name
+            var popis2 = data.current.weather[0];//array
+            // console.log(popis2);
+            popis = popis2.description; //notes
+            var tempK = data.current.temp - 273.15;//Kelvin
+            temp = tempK.toFixed(2); //x.00
+            var current = timeConverter2(data.current.dt);
+            var currentdate = current[0];
+            var currentmonth = current[1];
+            datum = currentdate + '.' + (currentmonth + 1)
+            for (let index = 0; index < 48; index++) {
+                var time = timeConverter2(data.hourly[index].dt);
+                var date = time[0];
+                var month = time[1];
+                if (currentmonth == month) {
+                    if (currentdate == (date - 1)) {
+                        //tady je day+1
+                        day_plus_one_desc = data.hourly[index].weather[0].description;
+                        var temp_temp = data.hourly[index].temp - 273.15
+                        day_plus_one_temp = temp_temp.toFixed(2);
+                        datum1 = date + '.' + (month + 1)
+                    }
+                    if (currentdate == (date - 2)) {
+                        //tady je day+2
+                        day_plus_two_desc = data.hourly[index].weather[0].description;
+                        var temp_temp = data.hourly[index].temp - 273.15;
+                        day_plus_two_temp = temp_temp.toFixed(2);
+                        datum2 = date + '.' + (month + 1)
+
+                    }
+                }
+
+
+            }
+            getCity();
+
+        })
+
 }
 
 button_get.onclick = function (event) {
@@ -36,19 +152,18 @@ button_get.onclick = function (event) {
             lat = data.coord.lat;
             lon = data.coord.lon;
             mycoordinates = [lat, lon];
-            var popis2 = data.weather[0];//array
-            popis = popis2.description; //notes
-            var tempK = data.main.temp - 273.15;//Kelvin
-            temp = tempK.toFixed(2); //x.00
+            get7day(mycoordinates);
+            //  var popis2 = data.weather[0];//array
+            //  popis = popis2.description; //notes
+            //  var tempK = data.main.temp - 273.15;//Kelvin
+            // temp = tempK.toFixed(2); //x.00
             //city = getCity(coordinates);
-            getCity(mycoordinates);
             //tady budeme vypisovat do html
-            
-           // document.getElementById("history").innerHTML = text;
+            // document.getElementById("history").innerHTML = text;
             //document.getElementById("history").innerHTML = text;
-        
+
         })
-   
+
 
 }
 button_locate.onclick = function (event) {//get GPS and find by gps
@@ -63,23 +178,7 @@ button_locate.onclick = function (event) {//get GPS and find by gps
         lat = crd.latitude.toString();
         lon = crd.longitude.toString();
         mycoordinates = [lat, lon];
-        getCity(mycoordinates);
-
-        fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=daily&appid=' + API_key_weather + '&lang=cz')//daily 7 days only GPS
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                // var city_name = data.name; //city name
-                var popis = data.current.weather[0];//array
-                var popis2 = popis.description; //notes
-                var tempK = data.current.temp - 273.15;//Kelvin
-                var temp = tempK.toFixed(2); //x.00
-
-            })
-
-
-
-
+        get7day(mycoordinates);
         return;
 
     }
@@ -90,6 +189,8 @@ button_locate.onclick = function (event) {//get GPS and find by gps
 
     navigator.geolocation.getCurrentPosition(success, error, options);
 }
+
+   shower_history();
 
 
 /*
